03add8b205d3d016376890d4efb80109b4eeef62
josef_lp <jose.lp.felipe@gmail.com>
2026-05-08 10:07:11 -0300

    Extensão dos testes do if

--- algoritmos/comandos_de_decisao/comandoIf.swift
+++ algoritmos/comandos_de_decisao/comandoIf.swift
@@ -1,5 +1,21 @@
+
 var estaSol: Bool = true
+var estaChovendo: Bool = true
+var estaNublado: Bool = false
+var descricaoTempo: String = "Ensolarado"
 
 if estaSol{
-    print("O sol está visivel")
+    print("Sol visivel")
+}
+if estaSol && estaChovendo{
+    print("Chuva e Sol")
+}
+if !estaNublado{
+    print("Opa parece que está nublado")
+}
+if estaChovendo || estaSol {
+    print("Opa, tem sol ou chuva")
+}
+if descricaoTempo == "ensolarado"{
+    print("Sim, está ensolarado")
 }
