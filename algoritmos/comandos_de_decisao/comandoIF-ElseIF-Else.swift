83321e5bb2df4f44113b0532e3deeb441e51bd2f
josef_lp <jose.lp.felipe@gmail.com>
2026-05-08 10:42:42 -0300

    adicao de mais dois arquivos de estudo, e a renomeacao de um dos arquivos

---
+++ algoritmos/comandos_de_decisao/comandoIF-ElseIF-Else.swift
@@ -0,0 +1,11 @@
+var idadePessoa: Int = 20
+
+if idadePessoa<=3{
+    print("Um bebê de \(idadePessoa) anos")
+}else if 3<idadePessoa && idadePessoa<=10{
+    print("Uma criança de \(idadePessoa) anos")
+}else if 11<=idadePessoa && idadePessoa<=17{
+    print("Um adolescente de \(idadePessoa) anos")
+}else{
+print("Um adulto")
+}
---
+++ algoritmos/comandos_de_decisao/comandoSwitch.swift
@@ -0,0 +1,20 @@
+var nota: Int = 100
+
+switch nota {
+    case 2:
+            print("Nota \(nota)")
+    case 5:
+            print("Nota \(nota)")
+    case 10:
+            print("Nota \(nota)")
+    case 20:
+            print("Nota \(nota)")
+    case 50:
+            print("Nota \(nota)")
+    case 100:
+            print("Nota \(nota)")
+    case 200:
+            print("Nota \(nota)")
+    default: 
+        print("Nota inválida")
+}
