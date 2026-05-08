3f6bc114fd419a4a5da961fe049fbe6af5ead6f0
josef_lp <jose.lp.felipe@gmail.com>
2026-05-08 10:23:13 -0300

    adicao do exercicio de if-else

---
+++ algoritmos/comandos_de_decisao/comandoIF-Else.swift
@@ -0,0 +1,14 @@
+let pi: Double = 3.14
+var numero: Double = 3.2
+
+if pi>numero{
+    print("número pi(\(pi)) é maior que \(numero)" )
+}else {
+    print("número pi(\(pi) é maior que \(numero)")
+
+numero = 3
+}
+if pi>numero{
+    print("número pi(\(pi)) é maior que \(numero)" )
+}else {
+    print("número pi(\(pi) é maior que \(numero)")}
